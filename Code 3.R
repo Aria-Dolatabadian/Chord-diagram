545128eabd3a7e8b8e84c397bc51fea614a894c1
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-11-14 15:29:43 +0800

    Update Code 3.R

--- Code 3.R
+++ Code 3.R
@@ -1,21 +1,35 @@
-Circ<-read.csv("data.csv",
-               header = TRUE,row.names =1 )
+library(circlize)
+
+# Read data from CSV
+Circ <- read.csv("data.csv", header = TRUE, row.names = 1)
 print(Circ)
 str(Circ)
-require(circlize)
+
+# Convert data to matrix
 Circ <- as.matrix(Circ)
+
+# Set the resolution of the output plot
+png("output_plot.png", width = 5000, height = 5000)  # Adjust width and height as needed
+
+# Plot the chord diagram
 chordDiagram(Circ)
-#This code clear the Data lebel of the Outer circos diagramm.
+
+# Clear the data label of the Outer circos diagram
 chordDiagram(Circ, annotationTrack = "grid", preAllocateTracks = 1)
-#add the labels and axis
+
+# Add labels and axis
 circos.trackPlotRegion(track.index = 2, panel.fun = function(x, y) {
   xlim = get.cell.meta.data("xlim")
   ylim = get.cell.meta.data("ylim")
   sector.name = get.cell.meta.data("sector.index")
-#print labels 
-circos.text(mean(xlim), ylim[1] + 2.5, sector.name, 
+  
+  # Print labels 
+  circos.text(mean(xlim), ylim[1] + 2.5, sector.name, 
               facing = "clockwise", niceFacing = TRUE, adj = c(0, 0.5),
-              cex=0.6)
-circos.axis(h = "top", labels.cex = 0.5, major.tick.percentage = 0.2, 
-            sector.index = sector.name, track.index = 2)
+              cex = 5) #labels font size
+  circos.axis(h = "top", labels.cex = 0.5, major.tick.percentage = 0.2, 
+              sector.index = sector.name, track.index = 2)
 }, bg.border = NA)
+
+# Save the plot
+dev.off()
